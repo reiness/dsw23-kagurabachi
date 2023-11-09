d45e7a334c2e947f5c0d2ebaa6752c56ad36cc17
ridhopandhu <rdhpndh@gmail.com>
2023-11-09 09:03:04 +0700

    improving markdowns

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 1. Create new folder `.streamlit`
 2. Inside that folder, create new file `secrets.toml`
 3. Insert this line `OPENAI_API_KEY = '<YOUR-API-KEY>'`
+
+
+# Our Recommended Streamlit Config
+1. Create new file inside `.streamlit` folder with the name `config.toml`
+2. Insert these lines of code
+```
+primaryColor="#F63366"
+backgroundColor="#FFFFFF"
+secondaryBackgroundColor="#F0F2F6"
+textColor="#262730"
+font="sans serif"
+```
+3. Re-run your streamlit app
--- app.py
+++ app.py
@@ -166,24 +166,7 @@ def chat_tab():
     st.header("Hello, I'm Kagura-Bot!")
     st.image('maid.png',width=400)
     st.write("Ask me Anything ^ ^")
-    # with st.sidebar:
-    #     st.subheader('Dataset information')
-    #     info = pd.DataFrame(
-    #         {
-    #             'Rows' : [df.shape[0]],
-    #             'Columns': [df.shape[1]]
-    #         }
-    #     )
-    #     st.dataframe(info, use_container_width=True, hide_index=True)
-
-    #     missing_values = pd.DataFrame(
-    #         df.isnull().sum().to_frame('Nulls').reset_index()
-    #     ).rename(columns={'index':'Column'})
-
-    #     st.dataframe(missing_values, use_container_width=True, hide_index=True)
-
-    # nrows = st.number_input("Show number of rows", min_value=1, value=5, step=1)
-    # st.dataframe(df.head(nrows), use_container_width=True)
+    
 
     with st.form("prompt_area"):
         prompt = st.text_input("Ask here")
