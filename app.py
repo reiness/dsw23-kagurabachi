dbb430841ef0cae00512d7cfa02961353f8943c7
Firdaus Dharma <firdausdharma@gmail.com>
2023-11-10 20:54:48 +0700

    handle error

--- app.py
+++ app.py
@@ -206,26 +206,20 @@ def statistic_test_tab():
     st.header('Statistic Tests')
     st.write('---')
 
-def preprocess_data(df):
-    df.drop(columns=['Customer ID', 'Longitude', 'Latitude'], inplace=True)
-
-    le = LabelEncoder()
-
-    for col in df.columns:
-        if df[col].dtype == 'object':
-            df[col] = le.fit_transform(df[col])
-
-    return df
-
 def model_tab():
     st.header('Model')
     st.write('---')
 
     # Load data
     df = pd.read_excel('data.xlsx')
-
+    
     # Preprocess data
-    df = preprocess_data(df)
+    df.drop(columns=['Customer ID', 'Longitude', 'Latitude'], inplace=True)
+    le = LabelEncoder()
+
+    for col in df.columns:
+        if df[col].dtype == 'object':
+            df[col] = le.fit_transform(df[col])
 
     # Split data
     X_train, X_test, y_train, y_test = train_test_split(df.drop(columns=['Churn Label']), df['Churn Label'], test_size=0.2, random_state=42, stratify=df['Churn Label'])
@@ -283,7 +277,12 @@ def model_tab():
         'Monthly Purchase (Thou. IDR)': [monthly_purchase],
         'CLTV (Predicted Thou. IDR)': [cltv]
     })
-    input_data = preprocess_data(input_data)
+
+    # Transformasi label encoding pada input_data
+    for col in input_data.columns:
+        if input_data[col].dtype == 'object':
+            input_data[col] = le.transform(input_data[col])
+
     input_data[numeric] = scaler_power.transform(input_data[numeric])
 
     # Make Prediction
