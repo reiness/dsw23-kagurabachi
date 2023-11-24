54a05c9101e7ff06e9d1851fcc9a71b98fd84cd5
Firdaus Dharma <firdausdharma@gmail.com>
2023-11-24 09:24:25 +0700

    repair EDA Tab

--- app.py
+++ app.py
@@ -46,150 +46,170 @@ st.set_page_config(
 
 # Define the EDA tab
 def eda_tab():
-    st.header("Exploratory Data Analysis")
-    st.write('---')
+    # st.header("Exploratory Data Analysis")
+    # st.write('---')
     data = pd.read_excel('data.xlsx')
 
-    st.subheader("DataFrame Info")
-    nrows = st.number_input("Show number of rows", min_value=1, value=5, step=1)
-    st.dataframe(data.head(nrows), use_container_width=True)
+    # st.subheader("DataFrame Info")
+    # nrows = st.number_input("Show number of rows", min_value=1, value=5, step=1)
+    # st.dataframe(data.head(nrows), use_container_width=True)
     
-    # Capture the .info() output into a string
-    buffer = io.StringIO()
-    data.info(buf=buffer)
-    info_text = buffer.getvalue()
-    buffer.close()
+    # # Capture the .info() output into a string
+    # buffer = io.StringIO()
+    # data.info(buf=buffer)
+    # info_text = buffer.getvalue()
+    # buffer.close()
     
-    # Display .info() of the DataFrame as Markdown
-    with st.expander("Click here to see the detailed information"):
-        st.markdown(f"```{info_text}```", unsafe_allow_html=True)
+    # # Display .info() of the DataFrame as Markdown
+    # with st.expander("Click here to see the detailed information"):
+    #     st.markdown(f"```{info_text}```", unsafe_allow_html=True)
 
-    st.subheader("General Churn Rate")
-    fig = px.pie(data.groupby('Churn Label')['Customer ID'].nunique().reset_index(), 
-         values='Customer ID', 
-         names='Churn Label')
-    st.plotly_chart(fig)
+    with st.container():
 
-    st.subheader("Map of Churn Rates by Hexagon")
-    hex_level = 5
-    data['hex_id'] = data.apply(lambda x: h3.geo_to_h3(x['Latitude'], x['Longitude'], hex_level), axis=1)
-    churn = data.assign(churn_clients = np.where(data['Churn Label']=='Yes',data['Customer ID'],None)).groupby(['hex_id']).agg({'churn_clients':'count'}).reset_index()
-    clients = data.groupby(['hex_id'])['Customer ID'].count().reset_index()
-    churn_data = clients.join(churn.set_index(['hex_id']), on=['hex_id'])
-    churn_data['churn_rate'] = churn_data['churn_clients']/churn_data['Customer ID']
-    churn_data['center'] = churn_data['hex_id'].apply(lambda x: h3.h3_to_geo(x))
+        col1, col2 = st.columns([0.6,0.4])
+        with col1:
+            st.subheader("Map of Churn Rates by Hexagon")
+            hex_level = 5
+            data['hex_id'] = data.apply(lambda x: h3.geo_to_h3(x['Latitude'], x['Longitude'], hex_level), axis=1)
+            churn = data.assign(churn_clients = np.where(data['Churn Label']=='Yes',data['Customer ID'],None)).groupby(['hex_id']).agg({'churn_clients':'count'}).reset_index()
+            clients = data.groupby(['hex_id'])['Customer ID'].count().reset_index()
+            churn_data = clients.join(churn.set_index(['hex_id']), on=['hex_id'])
+            churn_data['churn_rate'] = churn_data['churn_clients']/churn_data['Customer ID']
+            churn_data['center'] = churn_data['hex_id'].apply(lambda x: h3.h3_to_geo(x))
 
-    color_range = [churn_data['churn_rate'].min(), churn_data['churn_rate'].max()]
-    colormap = LinearColormap(["green", "orange", "red"], vmin=min(color_range), vmax=max(color_range))
+            color_range = [churn_data['churn_rate'].min(), churn_data['churn_rate'].max()]
+            colormap = LinearColormap(["green", "orange", "red"], vmin=min(color_range), vmax=max(color_range))
 
-    mean_lat, mean_lon = churn_data['center'].apply(lambda x: x[0]).mean(), churn_data['center'].apply(lambda x: x[1]).mean()
-    map_center = [mean_lat, mean_lon]
+            mean_lat, mean_lon = churn_data['center'].apply(lambda x: x[0]).mean(), churn_data['center'].apply(lambda x: x[1]).mean()
+            map_center = [mean_lat, mean_lon]
 
-    # Create a map
-    m = folium.Map(location=map_center, zoom_start=9, width='100%', height='80%', tiles='OpenStreetMap')
+            # Create a map
+            m = folium.Map(location=map_center, zoom_start=9, width='100%', height='100%', tiles='OpenStreetMap')
 
-    for _, row in churn_data.iterrows():
-        folium.Polygon(
-            locations=h3.h3_to_geo_boundary(row['hex_id']),
-            fill=True,
-            fill_color=colormap(row['churn_rate']),
-            fill_opacity=0.7,
-            stroke=False,
-            tooltip=f"Churn rate: {row['churn_rate']}<br>Number of customers: {row['Customer ID']}"
-        ).add_to(m)
+            for _, row in churn_data.iterrows():
+                folium.Polygon(
+                    locations=h3.h3_to_geo_boundary(row['hex_id']),
+                    fill=True,
+                    fill_color=colormap(row['churn_rate']),
+                    fill_opacity=0.7,
+                    stroke=False,
+                    tooltip=f"Churn rate: {row['churn_rate']}<br>Number of customers: {row['Customer ID']}"
+                ).add_to(m)
 
-    colormap.caption = 'Churn rate'
-    m.add_child(colormap)
+            colormap.caption = 'Churn rate'
+            m.add_child(colormap)
 
-    
-    st.write("This map visualizes churn rates by hexagon.")
-    st.write("Green represents lower churn rates, while red indicates higher churn rates.")
-    components.html(m._repr_html_(), height=700)
+            
+            # st.write("This map visualizes churn rates by hexagon.")
+            st.write("Green represents lower churn rates, while red indicates higher churn rates.")
+            components.html(m._repr_html_(), height=500)
+            
+        with col2:
+            st.subheader("General Churn Rate")
+            fig = px.pie(data.groupby('Churn Label')['Customer ID'].nunique().reset_index(), 
+                values='Customer ID', 
+                names='Churn Label')
+            fig.update_layout(width=500,height=370)
+            st.plotly_chart(fig)
+            with st.expander("**Problem Statement**"):
+                st.write("Sebagian besar pelanggan tetap setia (73.5%), sementara hanya sebagian kecil yang berhenti (26,5%). Tantangannya, bagaimana meminimalisir agar yang berhenti dapat tetap setia")
 
+    with st.container():
 
-    st.subheader("Customer's lifetime in the service")
-    fig = px.histogram(data, x="Tenure Months", color="Churn Label",marginal="box" )
-    fig.update_layout(
-        width=1100,  # Adjust the width
-        height=900,  # Adjust the height
-    )
-    st.plotly_chart(fig)
+        col3, col4, col5 = st.columns([0.45,0.275,0.275])
+        with col3:
+            st.subheader("Customer's lifetime in the service")
+            fig = px.histogram(data, x="Tenure Months", color="Churn Label",marginal="box" )
+            fig.update_layout(height=500)
+            st.plotly_chart(fig)
 
-    with st.expander("Click here to see the 'Churn Label' statistics"):
-        st.subheader("Quantiles of Tenure Months")
-        quantiles = data.groupby('Churn Label')['Tenure Months'].quantile([0.50, 0.75, 0.90, 0.95])
-        st.write(quantiles)
+        with col4:
+            st.subheader("What type of clients' devices who left the service?")
+            fig = px.pie(
+                data.groupby(['Device Class', 'Churn Label'])['Customer ID'].count().reset_index(),
+                values='Customer ID',
+                facet_col='Churn Label',
+                names='Device Class',
+                facet_col_wrap=1,hole=.5
+            )
+            fig.update_layout(width=350,height=500)
+            st.plotly_chart(fig)
+        
+        with col5:
+            st.subheader("What type of clients' payment method who left the service?")
+            fig = px.pie(data.groupby(['Payment Method','Churn Label'])['Customer ID'].count().reset_index(), 
+                    values='Customer ID', 
+                    facet_col = 'Churn Label',
+                    names='Payment Method',
+                    hole = .5,facet_col_wrap=1
+                    )
+            fig.update_layout(
+                width=350, height=500
+            )
+            st.plotly_chart(fig)
+    with st.container():
+        col6, col9 = st.columns([0.9999,0.0001])
+        with col6:
+            st.subheader('Churn rate by customer payment method')
+            fig = px.pie(data.groupby(['Payment Method','Churn Label'])['Customer ID'].count().reset_index(), 
+                    values='Customer ID', 
+                    names='Churn Label',
+                    facet_col = 'Payment Method',
+                    color = 'Churn Label',
+                    )
+            fig.update_layout(
+                width=1200,  # Adjust the width
+                # height=800,  # Adjust the height
+                margin=dict(l=50, r=50, b=50, t=50, pad=4),
+            )
+            st.plotly_chart(fig)
+        
 
-        st.subheader("Mean of Tenure Months")
-        means = data.groupby('Churn Label')['Tenure Months'].mean()
-        st.write(means)
+    with st.container():
+        col7, col8 = st.columns([.5 , .5])
+        with col7:
+            st.subheader("Correlation between Feature")
+            corr_df = data.copy()
+            corr_df['Churn Label'].replace(to_replace='Yes', value=1, inplace=True)
+            corr_df['Churn Label'].replace(to_replace='No',  value=0, inplace=True)
+            column_subset = corr_df[['Churn Label','Device Class', 'Games Product', 'Music Product', 'Education Product', 'Call Center', 'Video Product', 'Use MyApp', 'Payment Method']]
+            df_dummies = pd.get_dummies(column_subset, dtype=int)
+            # Create a Plotly heatmap
+            fig = go.Figure(data=go.Heatmap(
+                z=df_dummies.corr(),
+                x=df_dummies.columns,
+                y=df_dummies.columns,
+                colorscale='picnic',
+            ))
 
+            fig.update_layout(
+                height=600,  # Adjust the height as needed
+            #     width=500,   # Adjust the width as needed
+            )
+            # Display the Plotly heatmap using st.plotly_chart
+            st.plotly_chart(fig)
 
-    st.subheader("Services used by Client")
-    corr_df = data.copy()
-    corr_df['Churn Label'].replace(to_replace='Yes', value=1, inplace=True)
-    corr_df['Churn Label'].replace(to_replace='No',  value=0, inplace=True)
-    column_subset = corr_df[['Churn Label','Device Class', 'Games Product', 'Music Product', 'Education Product', 'Call Center', 'Video Product', 'Use MyApp', 'Payment Method']]
-    df_dummies = pd.get_dummies(column_subset, dtype=int)
-    # Create a Plotly heatmap
-    fig = go.Figure(data=go.Heatmap(
-        z=df_dummies.corr(),
-        x=df_dummies.columns,
-        y=df_dummies.columns,
-        colorscale='picnic',
-    ))
-
-    # Adjust the size of the heatmap within the layout
-    fig.update_layout(
-        width=1100,  # Adjust the width
-        height=900,  # Adjust the height
-    )
-    # Display the Plotly heatmap using st.plotly_chart
-    st.plotly_chart(fig)
-
-    fig = px.bar(df_dummies.corr()['Churn Label'].sort_values(ascending = False), 
+        with col8:
+            st.subheader("Detail Information From Heatmap")
+            fig = px.bar(df_dummies.corr()['Churn Label'].sort_values(ascending = False), 
              color = 'value')
+            
+            fig.update_layout(
+                # width=1100,  # Adjust the width
+                height=600,  # Adjust the height
+            )
+            st.plotly_chart(fig)
+
+
     
-    fig.update_layout(
-        width=1100,  # Adjust the width
-        height=900,  # Adjust the height
-    )
-    st.plotly_chart(fig)
+    # with st.expander("Click here to see the 'Churn Label' statistics"):
+    #     st.subheader("Quantiles of Tenure Months")
+    #     quantiles = data.groupby('Churn Label')['Tenure Months'].quantile([0.50, 0.75, 0.90, 0.95])
+    #     st.write(quantiles)
 
-    fig = px.pie(data.groupby(['Device Class','Churn Label'])['Customer ID'].count().reset_index(), 
-             values='Customer ID', 
-             facet_col = 'Churn Label',
-             names='Device Class',
-            title = "What type of clients' devices who left the service?")
-    fig.update_layout(
-        width=1000,  # Adjust the width
-        height=800,  # Adjust the height
-    )
-    st.plotly_chart(fig)
-
-    fig = px.pie(data.groupby(['Payment Method','Churn Label'])['Customer ID'].count().reset_index(), 
-             values='Customer ID', 
-             facet_col = 'Churn Label',
-             names='Payment Method',
-             hole = .5,
-            title = "What type of clients' payment method who left the service?")
-    fig.update_layout(
-        width=1000,  # Adjust the width
-        height=800,  # Adjust the height
-    )
-    st.plotly_chart(fig)
-
-    fig = px.pie(data.groupby(['Payment Method','Churn Label'])['Customer ID'].count().reset_index(), 
-            values='Customer ID', 
-            names='Churn Label',
-            facet_col = 'Payment Method',
-            color = 'Churn Label',
-            title = 'Churn rate by customer payment method')
-    fig.update_layout(
-        width=1000,  # Adjust the width
-        height=800,  # Adjust the height
-    )
-    st.plotly_chart(fig)
+    #     st.subheader("Mean of Tenure Months")
+    #     means = data.groupby('Churn Label')['Tenure Months'].mean()
+    #     st.write(means)
 
 def chat_tab():
     # plt.use('TkAgg')
